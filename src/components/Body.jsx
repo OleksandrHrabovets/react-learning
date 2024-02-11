6e4df0c2f67575dc771f858202432b1c10561131
Oleksandr Hrabovets <alex6754035@gmail.com>
2024-02-11 08:28:21 +0200

    add input

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,7 +1,14 @@
 function Body() {
+  function onClick() {
+    console.log("Login click");
+  }
+
   return (
     <div>
       <h3>Body</h3>
+      <input type="text" placeholder="login" />
+      <input type="text" placeholder="password" />
+      <button onClick={onClick}>Login</button>
     </div>
   );
 }
