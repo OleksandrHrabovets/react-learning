f5a816291136fce205ae927d162ccc6b55ed9951
Oleksandr Hrabovets <alex6754035@gmail.com>
2024-02-12 14:23:39 +0200

    add useState for auth

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,14 +1,28 @@
+import { useState } from "react";
+
 function Body() {
-  function onClick() {
+  const [auth, setAuth] = useState(true);
+
+  function onClickLogin() {
     console.log("Login click");
+    setAuth(!auth);
   }
 
   return (
     <div>
       <h3>Body</h3>
-      <input type="text" placeholder="login" />
-      <input type="text" placeholder="password" />
-      <button onClick={onClick}>Login</button>
+      {auth && (
+        <>
+          <input type="text" placeholder="login" />
+          <input type="text" placeholder="password" />
+          <button onClick={onClickLogin}>Login</button>
+        </>
+      )}
+      {!auth && (
+        <>
+          <button onClick={onClickLogin}>Logout</button>
+        </>
+      )}
     </div>
   );
 }
